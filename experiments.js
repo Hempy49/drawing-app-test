b4a99f9f2e10dda3db6d4a182b7577feaf447bba
Simon Hempenstall <simonjhemp@hotmail.com>
2017-08-13 22:22:08 +0100

    Spike initial functionality

---
+++ experiments.js
@@ -0,0 +1,48 @@
+// function draw(x, y) {
+  // var canvas = document.getElementById('canvas');
+  // canvas.width = window.innerWidth;
+  // canvas.height = window.innerHeight;
+  // var ctx = canvas.getContext('2d');
+
+  // const clearButton = document.querySelector('#clear');
+  // ctx.fillRect(100,100,100,100)
+  // ctx.fillRect(50,50,100,100)
+  // ctx.fillRect(5,5,100,100)
+  // ctx.fillRect(700,400, 100,100)
+  // ctx.beginPath();
+  // ctx.arc(500,75,50,0,2*Math.PI)
+  // // ctx.stroke();
+  // this.ctx.fillStyle = 'black';
+  // this.ctx.fill();
+  // this.ctx.closePath();
+  // ctx.save();
+//   ctx.clearRect(0,0,550,400);
+//   ctx.fillStyle = "rgba(0,200,0,1)";
+//   ctx.fillRect = (x,20, 50,50);
+//   ctx.restore();
+//   x += 5;
+//   var loopTimer = setTimeout('draw('+x+','+y+')', 200)
+// }
+//
+//
+
+// for (var i = 0; i < 100; i++) {
+//   var x = Math.random() * window.innerWidth;
+//   var y = Math.random() * window.innerHeight;
+//   ctx.beginPath();
+//   ctx.arc(x,y,50,0,2*Math.PI)
+//   // ctx.stroke()
+//   var color = ['blue', 'green', 'purple', 'gold'];
+//   color = color[Math.floor(Math.random()*color.length)]
+//   this.ctx.fillStyle = color;
+//   this.ctx.fill();
+//   this.ctx.closePath();
+// }
+//
+// function clear() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+// }
+// //
+// clearButton.addEventListener('click', clear);
+//
+// console.log("Hello");
---
+++ test.js
@@ -0,0 +1,22 @@
+// var md = false;
+// var canvas = document.getElementById('canvas')
+// canvas.addEventListener('mousedown', down);
+// canvas.addEventListener('mouseup', toggledraw);
+// canvas.addEventListener('mousemove', )
+// function (evt) {
+//   var mousePos = getMousePos(canvas, evt)
+// }
+// function down() {
+//   md = true;
+// }
+// function toggledraw() {
+//   md = false;
+// }
+//
+// function getMousePos(canvas, evt) {
+//   var rect = canvas.getBoundingClientRect();
+//   return{
+//     x:evt.clientX ,
+//     y:evt.clientY
+//   }
+// }
